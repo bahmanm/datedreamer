3a94981a8a77f6bb8867811ea1f8a122c8dd6f6a
Bahman Movaqar <bahman@bahmanm.com>
2017-10-21 09:40:11 +0200

    implement write plot to file

--- src/main/groovy/com/bahmanm/datedreamer/DateDreamer.groovy
+++ src/main/groovy/com/bahmanm/datedreamer/DateDreamer.groovy
@@ -5,6 +5,9 @@ import static java.lang.Math.*
 import org.apache.commons.math3.complex.Complex
 import javax.swing.JFrame
 import java.awt.Color
+import javax.imageio.ImageIO
+import java.awt.image.BufferedImage
+import java.awt.Graphics2D
 import org.math.plot.*
 
 /**
@@ -65,7 +68,18 @@ class DateDreamer {
   }
 
   private void writePlotToFile(Plot2DPanel plot, String filePath) {
-    //
+    def dims = plotDims.collect { it * 4 }
+    def img = new BufferedImage(dims[0], dims[1], BufferedImage.TYPE_INT_RGB)
+    plot.with {
+      setSize(dims[0], dims[1])
+      doLayout()
+      paint(img.createGraphics())
+    }
+    try {
+      ImageIO.write(img, 'png', new File(filePath));
+    } catch (Exception e) {
+      println("\nERROR: Failed to write plot to file --${e.message}.")
+    }
   }
 
   private void showPlot(Plot2DPanel plot) {
@@ -90,7 +104,7 @@ class DateDreamer {
     def plot = prepPlot()
     if (conf.mode in [Config.OutputMode.FILE, Config.OutputMode.BOTH]) {
       print('Writing the plot to file...')
-      writePlotToFile(plot, config.filePath)
+      writePlotToFile(plot, conf.filePath)
       println('done.')
     }
     if (conf.mode in [Config.OutputMode.UI, Config.OutputMode.BOTH]) {
@@ -101,9 +115,10 @@ class DateDreamer {
 
   static void generate() {
     def date = new Date()
-    new DateDreamer(date.year-100, date.month+1, date.date).doGenerate(
-      new Config(mode: Config.OutputMode.UI)
-    )
+    new DateDreamer(date.year-100, date.month+1, date.date)
+      .doGenerate(
+        new Config(mode: Config.OutputMode.FILE, filePath: '/home/bahman/Temp/t.png')
+      )
   }
 
 }
