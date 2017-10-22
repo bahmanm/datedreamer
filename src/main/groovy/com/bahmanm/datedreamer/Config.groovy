7c15d7bae254bcd421b753cb2891dadaf5bc254d
Bahman Movaqar <bahman@bahmanm.com>
2017-10-22 13:04:45 +0200

    read configurations from YAML file

--- src/main/groovy/com/bahmanm/datedreamer/Config.groovy
+++ src/main/groovy/com/bahmanm/datedreamer/Config.groovy
@@ -1,6 +1,8 @@
 package com.bahmanm.datedreamer
 
 
+import org.yaml.snakeyaml.Yaml
+
 /**
  * All of Date Dreamer configuration options.
  *
@@ -19,6 +21,8 @@ class Config {
   int nPoints
   int leap
 
+  private Config() {}
+
   private void setOutputMode(OutputMode outputMode) {
     this.outputMode = outputMode
   }
@@ -34,7 +38,32 @@ class Config {
   private void setLeap(int leap) {
     this.leap = leap
   }
+
+  //////////////////////////////////////////////////////////////////////////////
+  static Config fromYamlText(String text) {
+    def yaml = new Yaml().load(text)
+    def config = new Config()
+    config.with {
+      outputMode = Config.OutputMode.valueOf(
+	yaml.outputMode.toUpperCase()
+      )
+      filePath = yaml.filePath as String
+      nPoints = yaml.nPoints as int
+      leap = yaml.leap as int      
+    }
+    config
+  }
   
-  private Config() {}
+  static Config fromYaml(String path) {
+    fromYamlText(new File(path).text)
+  }
+
+  static String validate(Config config) {
+    if (config.outputMode in [Config.OutputMode.FILE, Config.OutputMode.BOTH])
+      if (config.filePath == null) '`filePath` cannot be empty'
+    if (config.nPoints < 10) '`nPoints` cannot be less than 10'
+    if (config.leap < 0) '`leap` cannot be less than 0'
+    null
+  }
   
 }
--- src/main/groovy/com/bahmanm/datedreamer/Main.groovy
+++ src/main/groovy/com/bahmanm/datedreamer/Main.groovy
@@ -8,82 +8,14 @@ package com.bahmanm.datedreamer
  */
 class Main {
 
-  static Map parseArgs(CliBuilder cli, String[] args) {
-    def options = cli.parse(args)
-    if (options)
-      [file: options.file ?: null,
-       noUi: options.noUi ?: false,
-       nPoints: (options.n ?: 10_000) as int,
-       leap: (options.initialLeap ?: 3) as int,
-       help: options.help]
-    else
-      null
-  }
-
-  static CliBuilder setupCli() {
-    def cli = new CliBuilder(
-      usage: 'datedreamer [options]',
-      header: 'Options'
-    )
-    cli.f(
-      args: 1,
-      longOpt: 'file',
-      'path to store the plot (e.g. /home/foo/bar.png)'
-    )
-    cli._(
-      longOpt: 'noUi',
-      "don't show a plot viewer, e.g. in case you just need to save the plot to file. " +
-	"If used, you must pass the `--file` argument."
-    )
-    cli.n(
-      args: 1,
-      longOpt: 'nPoints',
-      'how many data points to use in the plot --an integer'
-    )
-    cli.l(
-      args: 1,
-      longOpt: 'initialLeap',
-      'the integer to start the plot with, e.g. with n=100 and l=50, the plot starts ' +
-	'at 50 and ends at 150 (as the input).'
-    )
-    cli.h(
-      args: 0,
-      longOpt: 'help',
-      'prints this message'
-    )
-    cli
-  }
-
-  static Config optionsToConfig(Map opts) {
-    def conf = new Config()
-    if (opts.file != null && opts.noUi == false)
-      conf.outputMode = Config.OutputMode.BOTH
-    else if (opts.file == null)
-      conf.outputMode = Config.OutputMode.UI
-    else
-      conf.mode = Config.OutputMode.FILE
-    conf.filePath = opts.file
-    conf.nPoints = opts.nPoints
-    conf.leap = opts.leap
-    conf
-  }
-
-  static boolean validOptions(Map opts) {
-    if (opts.help == true)
-      return false
-    else if (opts.file == null && opts.noUi == true)
-      false
-    else
-      true
-  }
-
   static void main(String[] args) {
-    def cli = setupCli()
-    def opts = parseArgs(cli, args)
-    if (validOptions(opts))
-      DateDreamer.generate(optionsToConfig(opts))
+    def confFile = args.length > 0 ? args[0] : 'datedreamer.yml'
+    def config = Config.fromYaml(confFile)
+    def validation = Config.validate(config)
+    if (validation == null)
+      DateDreamer.generate(config)
     else
-      println(cli.usage())
+      println("Invalid configuration option: [$validation]")
   }
 
 }
